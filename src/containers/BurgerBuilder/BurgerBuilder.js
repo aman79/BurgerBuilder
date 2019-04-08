a4a26eecc690dd607ec792f9210f8d427af64b4c
Aman Gupta <gupta-dxc@hpe.com>
2019-04-08 16:25:09 +0530

    added spinner

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Modal from '../../components/UI/Modal/Modal';
 import Auxx from '../../hoc/Auxx/Auxx';
 import axios from '../../axios-order';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICE = {
   salad: 0.5,
@@ -23,7 +24,8 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchasable: false,
-    purchasing: false
+    purchasing: false,
+    loading: false
   };
 
   updatePurchaseState(ingredients) {
@@ -81,6 +83,7 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
+    this.setState({ loading: true });
     const { ingredients, totalPrice } = this.state;
 
     const order = {
@@ -96,11 +99,17 @@ class BurgerBuilder extends Component {
     };
     axios
       .post('/orders.json', order)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
+      .then(response => this.setState({ loading: false, purchasing: false }))
+      .catch(error => this.setState({ loading: false, purchasing: false }));
   };
   render() {
-    const { ingredients, totalPrice, purchasable, purchasing } = this.state;
+    const {
+      ingredients,
+      totalPrice,
+      purchasable,
+      purchasing,
+      loading
+    } = this.state;
     const disabledInfo = {
       ...ingredients
     };
@@ -110,17 +119,24 @@ class BurgerBuilder extends Component {
       }
     }
 
+    let orderSummary = (
+      <OrderSummary
+        ingredients={ingredients}
+        purchaseCancelled={this.purchaseCancelHandler}
+        purchaseContinued={this.purchaseContinueHandler}
+        price={totalPrice}
+      />
+    );
+    if (loading) {
+      orderSummary = <Spinner />;
+    }
     // console.log('disabled', disabledInfo);
     return (
       <Auxx>
         <Modal show={purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary
-            ingredients={ingredients}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler}
-            price={totalPrice}
-          />
+          {orderSummary}
         </Modal>
+
         <Burger ingredients={ingredients} />
         <BuildControls
           ingredientsAdded={this.addIngredient}
